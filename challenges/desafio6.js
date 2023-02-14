f3bb4b4411c9ea8242832abf0d0435e6e6908fec
Leandro Paiva <leandroapaiva@yahoo.com>
2023-02-14 16:15:41 -0300

    ♻ Refactor: refatora requisito 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,5 +1,5 @@
   db.produtos
   .find({
-    $and: 
-    [{ curtidas: { $gt: 10, $lt: 100 } }],
-  }, { _id: 0, nome: 1, curtidas: 1 });
+    curtidas: { $gt: 10, $lt: 100 },
+  }, {
+    _id: 0, nome: 1, curtidas: 1 });
--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,11 +1,3 @@
-// db.produtos
-//   .find({
-//     $and: [
-//       { vendidos: { $ne: 50 } },
-//       { tags: { $exists: false } },
-//     ],
-//   }, { _id: 0, nome: 1, vendidos: 1 });
-
 db.produtos
   .find({ 
     vendidos: { $ne: 50 }, tags: { $exists: false },
