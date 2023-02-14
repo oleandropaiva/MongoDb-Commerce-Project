61fa84f56fb949b480620a470e82a0eeca582b7b
Leandro Paiva <leandroapaiva@yahoo.com>
2023-02-14 19:21:22 -0300

    ♻ Refactor: refatora organização do código

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,5 +1,7 @@
 db.produtos
-.updateMany({}, { $set: { criadoPor: "Ronald McDonald" } });
+.updateMany({},
+  { $set: { criadoPor: "Ronald McDonald" } });
 
 db.produtos
-.find({}, { _id: 0, nome: 1, criadoPor: 1 });
+.find({},
+  { _id: 0, nome: 1, criadoPor: 1 });
