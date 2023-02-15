b00f4fab21ba095dea4ed779db4f0b461f912fcf
Leandro Paiva <leandroapaiva@yahoo.com>
2023-02-14 23:20:47 -0300

    ✔ feat: Remove o item cebola de todos os sanduíches

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,7 +1,3 @@
-// Inclua bacon no final da lista de ingredientes dos sanduíches Big Mac e Quarteirão com Queijo
-// Crie uma query que faça a inclusão de bacon no final da lista de ingredientes dos sanduíches Big Mac e Quarteirão com Queijo
-// Crie uma query que retorne o nome e ingredientes de todos os documentos.
-
 db.produtos
   .updateMany(
     { nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
