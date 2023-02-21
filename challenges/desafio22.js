417cd90440231e01682ff0e0844edaca343d3b72
Leandro Paiva <leandroapaiva@yahoo.com>
2023-02-21 20:30:17 -0300

    ✔ feat: formata identacao

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,20 +1,23 @@
-db.produtos.updateMany(
-  {},
-  { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } },
-);
+db.produtos
+  .updateMany(
+    {},
+    { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } },
+  );
 
-db.produtos.updateOne(
-  { nome: "Big Mac" },
-  { $inc: { "vendasPorDia.3": 60 } },
-);
+db.produtos
+  .updateOne(
+    { nome: "Big Mac" },
+    { $inc: { "vendasPorDia.3": 60 } },
+  );
 
-db.produtos.updateMany(
-  { tags: "bovino" },
-  { $inc: { "vendasPorDia.6": 120 } },
-);
+db.produtos
+  .updateMany(
+    { tags: "bovino" },
+    { $inc: { "vendasPorDia.6": 120 } },
+  );
 
-db.produtos.find(
-  {},
-  { _id: 0, nome: 1, vendasPorDia: 1 },
-);
-// .
+db.produtos
+  .find(
+    {},
+    { _id: 0, nome: 1, vendasPorDia: 1 },
+  );
